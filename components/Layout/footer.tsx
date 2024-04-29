d1bd61f8d55222023914c51a0980e27c4f680790
MeGaOne47 <nguyentanhung9a1@gmail.com>
2024-04-30 00:33:10 +0700

    Update: client header and footer

--- components/Layout/footer.tsx
+++ components/Layout/footer.tsx
@@ -1,9 +1,24 @@
-const FooterComponent = () => {
+import React from 'react';
+import { FacebookOutlined, GithubOutlined, YoutubeOutlined } from '@ant-design/icons';
+
+const FooterComponent: React.FC = () => {
   return (
-    <footer style={{ textAlign: 'center'}}>
-      Ant Design ©2022 Created by Ant UED
+    <footer style={{ textAlign: 'center', backgroundColor: '#f0f2f5', padding: '20px 0', position: 'fixed', left: 0, bottom: 0, width: '100%'  }}>
+      <div>Ant Design ©2022 Created by Ant UED</div>
+      <div>
+        <a href="https://www.facebook.com/profile.php?id=100028399301933" target="_blank" rel="noopener noreferrer">
+          <FacebookOutlined />
+        </a>{" | "}
+        <a href="https://github.com/MeGaOne47" target="_blank" rel="noopener noreferrer">
+          <GithubOutlined />
+        </a>{" | "}
+        <a href="https://www.youtube.com/channel/UC2lPzjPXjeV2HoRPI2IJuiQ" target="_blank" rel="noopener noreferrer">
+          <YoutubeOutlined />
+        </a>
+      </div>
     </footer>
   );
 };
 
 export default FooterComponent;
+
--- components/Layout/header.tsx
+++ components/Layout/header.tsx
@@ -1,65 +1,34 @@
 "use client"
-import React, { useState } from 'react';
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
+import React from 'react';
 import { Menu } from 'antd';
-
-type MenuItem = Required<MenuProps>['items'][number];
-
-const items: MenuItem[] = [
-  {
-    label: 'Navigation One',
-    key: 'mail',
-    icon: <MailOutlined />,
-  },
-  {
-    label: 'Navigation Two',
-    key: 'app',
-    icon: <AppstoreOutlined />,
-    disabled: true,
-  },
-  {
-    label: 'Navigation Three - Submenu',
-    key: 'SubMenu',
-    icon: <SettingOutlined />,
-    children: [
-      {
-        type: 'group',
-        label: 'Item 1',
-        children: [
-          { label: 'Option 1', key: 'setting:1' },
-          { label: 'Option 2', key: 'setting:2' },
-        ],
-      },
-      {
-        type: 'group',
-        label: 'Item 2',
-        children: [
-          { label: 'Option 3', key: 'setting:3' },
-          { label: 'Option 4', key: 'setting:4' },
-        ],
-      },
-    ],
-  },
-  {
-    key: 'alipay',
-    label: (
-      <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-        Navigation Four - Link
-      </a>
-    ),
-  },
-];
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
 
 const HeaderComponent: React.FC = () => {
-  const [current, setCurrent] = useState('mail');
-
-  const onClick: MenuProps['onClick'] = (e) => {
-    console.log('click ', e);
-    setCurrent(e.key);
-  };
-
-  return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />;
+  return (
+    <Menu mode="horizontal">
+      <Menu.Item key="mail" icon={<MailOutlined />}>
+        Navigation One
+      </Menu.Item>
+      <Menu.Item key="app" icon={<AppstoreOutlined />} disabled>
+        Navigation Two
+      </Menu.Item>
+      <Menu.SubMenu key="SubMenu" icon={<SettingOutlined />} title="Navigation Three - Submenu">
+        <Menu.ItemGroup title="Item 1">
+          <Menu.Item key="setting:1">Option 1</Menu.Item>
+          <Menu.Item key="setting:2">Option 2</Menu.Item>
+        </Menu.ItemGroup>
+        <Menu.ItemGroup title="Item 2">
+          <Menu.Item key="setting:3">Option 3</Menu.Item>
+          <Menu.Item key="setting:4">Option 4</Menu.Item>
+        </Menu.ItemGroup>
+      </Menu.SubMenu>
+      <Menu.Item key="alipay">
+        <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+          Navigation Four - Link
+        </a>
+      </Menu.Item>
+    </Menu>
+  );
 };
 
 export default HeaderComponent;

522b1c699ba30d9429a4e3734f1272b911d2cbdb
MeGaOne47 <nguyentanhung9a1@gmail.com>
2024-04-29 21:18:47 +0700

    Update: url

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,14 @@ export const metadata: Metadata = {
     title: 'Deploy dự án',
     description: 'VJC thực tập',
     type: 'website',
+    images: [
+      {
+        url: 'https://www.vjp-connect.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogobanner.dcf00dbd.png&w=3840&q=75', // Đường dẫn tương đối đến hình ảnh
+        width: 1200,
+        height: 630,
+        alt: 'Mô tả cho hình ảnh',
+      },
+    ],
   },
   metadataBase: metadataBase,
 };

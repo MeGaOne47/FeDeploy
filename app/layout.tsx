b3d54595075d11c8c8dc1cfc998d874c4e81113d
MeGaOne47 <nguyentanhung9a1@gmail.com>
2024-04-29 20:24:31 +0700

    Update: update meta data on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,19 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
+export const openGraphImage = { images: ['https://www.vjp-connect.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogobanner.dcf00dbd.png&w=1200&q=75'] }
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Deploy dự án",
   description: "VJC thực tập",
+  openGraph: {
+    title: 'Học Nextjs 14',
+    description: 'Nâng cao kỹ năng lập trình và tiến bộ hơn trong tương lai.',
+    type: 'website',
+    ...openGraphImage,
+  },
 };
 
 export default function RootLayout({

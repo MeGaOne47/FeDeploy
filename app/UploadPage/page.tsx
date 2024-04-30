7e797f0930d22b682a35dbce9825cbb51c6cbf05
MeGaOne47 <nguyentanhung9a1@gmail.com>
2024-04-30 21:02:42 +0700

    Update: update css select list file

--- app/UploadPage/page.tsx
+++ app/UploadPage/page.tsx
@@ -1,7 +1,5 @@
 "use client"
 import Select_File_Upload from '@/components/Upload/Select_File_Upload';
-import Button from 'antd/es/button';
-import Link from 'next/link';
 
 const UploadPage = () => {
 
@@ -9,14 +7,6 @@ const UploadPage = () => {
     <div style={{ padding: '20px' }}>
       <h1 style={{textAlign: 'center'}}>Hung Nguyen</h1>
       <Select_File_Upload/>
-      <Button 
-        type='dashed'
-        style={{ marginLeft: '10px' }}
-       >
-        <Link href="/">
-          Home
-        </Link>
-      </Button>
     </div>
   );
 };
--- components/Upload/Select_File_Upload.tsx
+++ components/Upload/Select_File_Upload.tsx
@@ -7,6 +7,7 @@ import { SERVER_DOMAIN } from '../../app/constants';
 import message from "antd/es/message";
 import Upload from "antd/es/upload";
 import Button from "antd/es/button";
+import Link from "next/link";
 
 function Select_File_Upload() {
     const [file, setFile] = useState<RcFile | null>(null);
@@ -39,22 +40,36 @@ function Select_File_Upload() {
 
     return(
         <>
-            <h2>Upload File</h2>
-            <Upload
-                accept=".png,.jpeg,.jpg"
-                beforeUpload={beforeUpload}
-            >
-                <Button icon={<UploadOutlined />}>Select File</Button>
-            </Upload>
+            <div style={{
+                margin: 'auto',
+                textAlign: 'center'
+            }}>
+                <h2>Upload File</h2>
+                <Upload
+                    accept=".png,.jpeg,.jpg"
+                    beforeUpload={beforeUpload}
+                >
+                    <Button icon={<UploadOutlined />}>Select File</Button>
+                </Upload>
 
-            <Button
-                type="primary"
-                style={{ marginTop: '10px' }}
-                onClick={handleUpload}
-                disabled={!file}
-            >
-                Upload
-            </Button>
+                <Button
+                    type="primary"
+                    style={{ marginTop: '10px' }}
+                    onClick={handleUpload}
+                    disabled={!file}
+                >
+                    Upload
+                </Button>
+
+                <Button 
+                    type='dashed'
+                    style={{ marginLeft: '10px' }}
+                >
+                    <Link href="/">
+                    Home
+                    </Link>
+                </Button>
+            </div>
         </>
     )
 }

5737cad95539258e34400b8216e49e6662822594
Nguyễn Tấn Hùng <nguyentanhung9a1@gmail.com>
2024-11-10 15:02:45 +0700

    update header mobie

--- components/Layout/header.tsx
+++ components/Layout/header.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 // import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
 // import Menu from 'antd/es/menu/menu';
 
@@ -48,21 +48,54 @@ const HeaderComponent: React.FC = () => {
   //     ),
   //   },
   // ], []);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     // <Menu mode="horizontal" items={menuItems} />
+    // <header className="bg-gray-800 text-white py-4 sticky top-0 z-50">
+    //   <nav className="max-w-7xl mx-auto flex justify-between items-center">
+    //     <div className="text-xl font-semibold">Nguyễn Tấn Hùng</div>
+    //     <ul className="flex space-x-6">
+    //       <li><a href="/" className="hover:text-gray-300">Trang chủ</a></li>
+    //       <li><a href="#GioiThieu" className="hover:text-gray-300">Giới thiệu</a></li>
+    //       <li><a href="#KyNang" className="hover:text-gray-300">Kỹ năng</a></li>
+    //       <li><a href="#KinhNghiem" className="hover:text-gray-300">Kinh nghiệm</a></li>
+    //       <li><a href="#DuAn" className="hover:text-gray-300">Dự án</a></li>
+    //     </ul>
+    //   </nav>
+    // </header>
     <header className="bg-gray-800 text-white py-4 sticky top-0 z-50">
-      <nav className="max-w-7xl mx-auto flex justify-between items-center">
-        <div className="text-xl font-semibold">Nguyễn Tấn Hùng</div>
-        <ul className="flex space-x-6">
+      <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
+        <div className="text-lg sm:text-xl font-semibold">Nguyễn Tấn Hùng</div>
+        <ul className="hidden sm:flex space-x-4 md:space-x-6">
           <li><a href="/" className="hover:text-gray-300">Trang chủ</a></li>
           <li><a href="#GioiThieu" className="hover:text-gray-300">Giới thiệu</a></li>
-          <li><a href="#KyNang" className="hover:text-gray-300">Kỹ năng</a></li>
-          <li><a href="#KinhNghiem" className="hover:text-gray-300">Kinh nghiệm</a></li>
-          <li><a href="#DuAn" className="hover:text-gray-300">Dự án</a></li>
+          <li><a href="#KyNang" className="hover:text-gray-300">Kỹ năng</a></li>
+          <li><a href="#KinhNghiem" className="hover:text-gray-300">Kinh nghiệm</a></li>
+          <li><a href="#DuAn" className="hover:text-gray-300">Dự án</a></li>
         </ul>
+        <div className="sm:hidden">
+          <button onClick={toggleMenu} aria-label="Mở menu" className="hover:text-gray-300">
+            <svg className="w-6 h-6" fill="#ffff" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50">
+              <path d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"></path>
+            </svg>
+          </button>
+        </div>
       </nav>
+      {isMenuOpen && (
+        <ul className="sm:hidden bg-gray-800 text-white flex flex-col items-center space-y-4 py-4">
+          <li><a href="/" className="hover:text-gray-300">Trang chủ</a></li>
+          <li><a href="#GioiThieu" className="hover:text-gray-300">Giới thiệu</a></li>
+          <li><a href="#KyNang" className="hover:text-gray-300">Kỹ năng</a></li>
+          <li><a href="#KinhNghiem" className="hover:text-gray-300">Kinh nghiệm</a></li>
+          <li><a href="#DuAn" className="hover:text-gray-300">Dự án</a></li>
+        </ul>
+      )}
     </header>
+
   );
 };
 

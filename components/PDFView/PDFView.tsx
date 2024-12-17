00b9ffd7164724d644a236fa4fc29c32a94d78f9
Nguyễn Tấn Hùng <nguyentanhung9a1@gmail.com>
2024-12-18 03:19:32 +0700

    change iframe google docs

--- components/PDFView/PDFView.tsx
+++ components/PDFView/PDFView.tsx
@@ -1,15 +1,14 @@
-'use client';
-import React from 'react';
+"use client";
+import React from "react";
 
 const PDFView = () => {
   return (
     <div>
-      <embed
-        src="https://be-deploy.vercel.app/view/duthibaocao.pdf"
+      <iframe
+        src="http://docs.google.com/gview?url=https://be-deploy.vercel.app/view/duthibaocao.pdf&embedded=true"
         width="100%"
-        height="1000vh"
-        type="application/pdf"
-      />
+        height="800px"
+      ></iframe>
     </div>
   );
 };

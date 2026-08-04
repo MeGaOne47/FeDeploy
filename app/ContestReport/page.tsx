1544caed0461ccb6368a817cc2db72361b73df3a
Nguyễn Tấn Hùng <nguyentanhung9a1@gmail.com>
2026-08-05 01:18:13 +0700

    remove module ContestReport

--- app/ContestReport/page.tsx
+++
@@ -1,9 +0,0 @@
-import ContestReport from "@/components/ContestReport/ContestReport";
-
-export default function ContestReportPage() {
-  return (
-    <div className="px-4 py-8 sm:px-6 lg:px-8">
-      <ContestReport />
-    </div>
-  );
-}

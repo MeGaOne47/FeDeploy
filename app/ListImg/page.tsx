8a8480eef5d68c005391977a47ea91862d724c02
Nguyễn Tấn Hùng <nguyentanhung9a1@gmail.com>
2026-08-10 13:20:09 +0700

    remove ListImage and UploadPage components

--- app/ListImg/page.tsx
+++
@@ -1,9 +0,0 @@
-import ListImage from "@/components/List_Image/List_Image";
-
-export default function ListImagePage() {
-  return (
-    <div className="px-4 py-8 sm:px-6 lg:px-8">
-      <ListImage />
-    </div>
-  );
-}
--- app/UploadPage/page.tsx
+++
@@ -1,9 +0,0 @@
-import SelectFileUpload from "@/components/Upload/Select_File_Upload";
-
-export default function UploadPage() {
-  return (
-    <div className="px-4 py-8 sm:px-6 lg:px-8">
-      <SelectFileUpload />
-    </div>
-  );
-}
